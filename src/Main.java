89d6f4cfa3c76df3713f1bbc5ac000dd91630716
Ellis <sc22es@leeds.ac.uk>
2025-11-04 19:58:01 +0000

    Initial

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] options = {"Rock", "Paper", "Scissors"};
+
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.println("Enter option: Rock:0, Paper:1, Scissors:2");
+
+        int option1=-1;
+
+        while (option1 < 0 || 2 < option1 ) {
+
+            option1 = Integer.parseInt(myObj.nextLine());
+
+            if (option1 < 0 || 2 < option1 ) {
+                System.out.println("Incorrect option");
+            }
+        }
+
+        int option2 = (int)(Math.random() * 3);
+        System.out.printf("%s (You) vs. %s%n", options[option1], options[option2]);
+
+        if (option1 == option2) {
+            System.out.println("Draw!");
+        }
+        else if (option1 == (option2+1)%3 ) {
+            System.out.println("You Win!");
+        }
+        else {
+            System.out.println("You Lose!");
+        }
+
+    }
+}
